e934f491903b0a8e079ed5c938bda2013aae8ae2
Selil <selilmcp@gmail.com>
2024-04-04 13:35:43 +0100

    test for product list fetching success case

--- B&WTests/ProductsListViewModelTests.swift
+++ B&WTests/ProductsListViewModelTests.swift
@@ -2,4 +2,33 @@ import XCTest
 @testable import B_W
 
 class ProductsListViewModelTests: XCTestCase {
+    
+    func testProductFetching_success(){
+        
+        let dummyProduct = Product(id: "1",
+                                   name: "product1",
+                                   description: "some description",
+                                   price: "20",
+                                   imagePath: "http://")
+        
+        let sut = DefaultProductsListViewModel(useCase: dummyGetProductUseCase(products: Products(products: [dummyProduct])),
+                                     actions: nil)
+        sut.viewDidLoad()
+        XCTAssertTrue(sut.products.count == 1)
+        XCTAssertEqual(dummyProduct.name, sut.products.first?.name)
+    }
+}
+
+class dummyGetProductUseCase:GetProductsUseCase{
+    
+    private let products:Products
+    
+    init(products: Products){
+        self.products = products
+    }
+    
+    func execute(requestValue: B_W.GetProductsUseCaseRequestValue, completion: @escaping (Result<B_W.Products, Error>) -> Void) -> B_W.Cancellable? {
+        completion(.success(products))
+        return nil
+    }
 }
